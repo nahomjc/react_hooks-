da0c23741946f090962f7bb3aacb91056493076d
nahomjc <82502560+nahomjc@users.noreply.github.com>
2023-04-29 17:20:43 +0300

    feat: useEffect

---
+++ src/UseEffect.js
@@ -0,0 +1,32 @@
+import React,{useEffect,useState} from 'react'
+
+const UseEffect = () => {
+
+    const[data, setData]=useState('')
+
+
+    useEffect(()=>{
+
+        const fetchData=async()=>{
+            const response = await fetch('http://api.example.com/data')
+            const json = await response.json()
+            setData(json)
+        }
+        fetchData()
+    },[])
+
+
+    if(!data){
+        return(<h1>Loading...</h1>)
+        
+    }
+  return (
+    <div>
+
+{data}
+
+    </div>
+  )
+}
+
+export default UseEffect
