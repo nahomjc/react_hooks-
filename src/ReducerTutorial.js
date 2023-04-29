012ed9ffb2a266ba4cf411d2126e273847a32f38
nahomjc <82502560+nahomjc@users.noreply.github.com>
2023-04-29 13:45:41 +0300

    Chore: name add

--- src/ReducerTutorial.js
+++ src/ReducerTutorial.js
@@ -1,32 +1,23 @@
 import React,{useReducer} from 'react'
 
 
-const reducer= (state, action)=>{
-
-  switch(action.type){
-case 'INCREMANT':
-  return{
-count: state.count+1, showText: state.showText
-  }
-  case 'toggle':
-    return{count: state.count , showText: !state.showText}
+const reducer=(state, action)=>{
+    switch(action.type){
+case 'INCREMENT':
+return {count: state.count+1, showText:state.showText}
+ case "ToggleShowText":
+    return{count: state.count, showText: !state.showText}
 default:
-return state
-  }
+return state}
 }
-
 function ReducerTutorial() {
 
-    const[state ,dispatch]=useReducer(reducer,{count: 0, showText: true})
+    const[state, dispatch]=useReducer(reducer,{count:0, showText:true})
 
   return (
-    <div> 
-   <h1>{state.count}</h1> 
-    <button onClick={()=>{dispatch({type:'INCREMANT'}); dispatch({type:'toggle'})}}>
-
-      Clicke me
-    </button>
-    <h1>{state.showText&& <p>show Text</p>}</h1>
+    <div> <h1>{state.count}</h1>
+    <button onClick={()=>{dispatch({type:"INCREMENT"}); dispatch({type:"ToggleShowText"})}}>Click me</button>
+    
     </div>
   )
 }
