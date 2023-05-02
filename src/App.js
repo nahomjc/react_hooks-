6f654df5bbc85bff3a0be8338f51815b8016bc51
nahomjc <82502560+nahomjc@users.noreply.github.com>
2023-05-02 12:05:12 +0300

    feat: useLayoutEffect added

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@
 import './App.css';
 import ReducerTutorial from './ReducerTutorial';
 import { InputRef } from './InputRef';
-
+import UseLayoutEffect from './UseLayoutEffect';
 function App() {
   return (
     <div className="App">
     
        <ReducerTutorial/>
        <InputRef/>
-   
+   <UseLayoutEffect/>
     </div>
   );
 }
